bcc3708a80b6572c6457ea8edef09c0f4fbb4db1
yunnyisgood <cyeon0801@gmail.com>
2021-07-11 23:32:29 +0900

    today commit

--- keras/keras07_boston.py
+++ keras/keras07_boston.py
@@ -1,8 +1,10 @@
-from sklearn.datasets import load_boston
-from tensorflow.python.keras.models import Sequential # 교육용 예제 임포트
+from sklearn.datasets import load_boston # 교육용 예제 임포트
+from tensorflow.python.keras.models import Sequential 
 from tensorflow.keras.layers import Dense
 import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 
 #1. data
 datasets = load_boston()
@@ -15,7 +17,8 @@ print(datasets.DESCR)
 
 print(datasets.feature_names)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, shuffle=True, random_state=66)
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+train_size=0.7, shuffle=True, random_state=66)
 
 # 2.model
 model = Sequential()
@@ -33,19 +36,28 @@ model.add(Dense(1))
 # 3. compile
 model.compile(loss='mse', optimizer="adam")
 
-model.fit(x_train, y_train, epochs=1000, batch_size=10)
+model.fit(x_train, y_train, epochs=100, batch_size=43)
+
 
 # 4. evaluate and predict
 loss = model.evaluate(x_test, y_test)
 print('loss:', loss)
 
+# loss: 76.62830352783203
+
 y_pred = model.predict(x_test)
-print('y의 예측값:', y_pred)
+print('x_test를 통한 y의 예측값:', y_pred)
 
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_pred) # y_test, y_pred 차이
 print('r2스코어:',r2)
 
+# r2스코어: 0.07248870680689357
+
+# 시각화 
+plt.scatter(y_test, y_pred)
+plt.plot(x, y_pred, color='red')
+plt.show()
 
 
 
---
+++ keras/keras07_boston_ver2.py
@@ -0,0 +1,71 @@
+from keras.datasets import boston_housing
+from tensorflow.python.keras.models import Sequential 
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+import numpy as np
+
+# data
+
+(x_train, y_train), (x_test, y_test)= boston_housing.load_data()
+
+# 정규화 과정 추가
+
+mean = x_train.mean(axis = 0)
+std = x_train.std(axis = 0)
+x_train = (x_train - mean)/std  # 훈련데이터 
+x_test = (x_test - mean)/std # 검증데이터 
+
+print(x_train.shape)  # (404, 13)
+print(y_train.shape) # (404, )
+print(x_test.shape) # (102, 13)
+print(y_test.shape) # (102, )
+
+
+# modeling
+model = Sequential()
+model.add(Dense(33, activation='relu', input_shape=(13,)))
+model.add(Dense(33, activation='relu'))
+model.add(Dense(12, activation='relu'))
+model.add(Dense(1))
+
+# compile
+model.compile(optimizer='adam', loss='mse', metrics='mae') # 오류나면 metrics 추가
+# 모델 훈련 과정을 측정하는 지표로는 mae를 사용
+# mse는 오차를 측정, 프로세스 자체를 측정할 때에는 mae를 사용
+
+# model 구조 요약
+model.summary()
+
+model.fit(x_train,y_train, epochs=100, batch_size=30 )
+
+
+# 4. evaluate and predict
+loss = model.evaluate(x_test, y_test)
+print('loss:', loss)
+
+# loss: [19.372793197631836, 2.867462158203125]
+
+y_pred = model.predict(x_test)
+print('y의 예측값:', y_pred)
+
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_pred) # y_test, y_pred 차이
+print('r2스코어:',r2)
+
+# r2스코어: 0.7597422462963858
+
+# 예측값과 실제값의 차이 시각화 
+plt.scatter(y_test, y_pred)
+plt.xlabel('True Values[1000$]')
+plt.ylabel('Predictions [1000$]')
+plt.axis('equal')
+plt.xlim(plt.xlim())
+plt.ylim(plt.ylim())
+_ = plt.plot([-10, 100], [-10, 100])
+plt.show()
+
+# 예측 오차 분포 
+plt.hist(loss, bins=50)
+plt.xlabel('Predictions error [1000$]')
+plt.ylabel('Count')
+plt.show()
