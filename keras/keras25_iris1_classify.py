1398695e36fd6c938b15a6b451ad63a98fc58f8d
yunnyisgood <cyeon0801@gmail.com>
2021-07-15 20:04:16 +0900

    today commit

--- keras/keras25_iris1_classify.py
+++ keras/keras25_iris1_classify.py
@@ -1,27 +1,41 @@
 import numpy as np
-from sklearn import datasets
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
-datasets = load_breast_cancer() # (569, 30)
+# 다중분류, One-Hot-Encoding 
 
+datasets = load_iris()
 print(datasets.DESCR)
 print(datasets.feature_names)
 
 x = datasets.data
 y = datasets.target
 
-print(x.shape) # (569, 30)
-print(y.shape) # (569,)
+print(x.shape)  # (150, 4)
+print(y.shape)  # (150,)
+print(y) # -> cancer와 마찬가지로 0, 1로 구성되어있다
 
-print(np.unique(y)) # y데이터는 0과 1데이터로만 구성되어 있다
+''' 
+One-Hot-Encoding (150, ) -> (150, 3, 1)
+0 -> [1, 0, 0]
+1 -> [0, 1, 0]
+2 -> [0, 0, 1]
 
+ex) [0, 1, 2, 1]
+=> [[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0]]
+=> (4, )에서 (4, 3)으로 바뀌었다
+
+-> 원래 y 데이터 (150, )에서 (150, 3)으로 바뀌게 됨을 알 수 있다
+'''
+y = to_categorical(y) # -> one-Hot-Encoding 하는 방법
+print(y[:5])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, random_state=9, shuffle=True)
 
@@ -34,16 +48,16 @@ scaler.transform(x_test)
 
 #modeling
 model = Sequential()
-model.add(Dense(1000, input_dim=30, activation='relu'))
+model.add(Dense(1000, input_shape=(4,), activation='relu'))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(60, activation='relu'))
 model.add(Dense(20, activation='relu'))
 model.add(Dense(10, activation='relu'))
-model.add(Dense(1, activation='sigmoid')) 
-# 도출되는 y값을 0과 1로 한정짓는다
+model.add(Dense(3, activation='softmax')) 
+# output 
 
 # compile
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 # loss=mse가 아닌 binary_crossentropy -> 이진분류 방법 
 
 
@@ -56,16 +70,7 @@ loss = model.evaluate(x_test, y_test) # evaluate는 metrics도 반환
 print('loss: ', loss[0])
 print('accuracy: ', loss[1])
 
-# y_pred = model.predict(x_test)
-# r2 = r2_score(y_test, y_pred)
-# print('r2 스코어: ', r2)
-
-# plt.plot(hist.history['loss'])
-# plt.plot(hist.history['val_loss'])
-
-# plt.title("loss, val_loss")
-# plt.xlabel("epochs")
-# plt.ylabel("loss, val_loss")
-# plt.legend(["train loss", "val_loss"])
-# plt.show()
-
+y_pred = model.predict(x_test[:5])
+print(y_test[:5])
+print('--------softmax를 통과한 값 --------')
+print(y_pred)
---
+++ keras/keras26_wine.py
@@ -0,0 +1,93 @@
+import numpy as np
+from sklearn.datasets import load_wine
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, PowerTransformer, QuantileTransformer
+
+'''
+완성
+accuracy 0.8 이상 만들어 볼 것
+=> 0.9279999732971191 가 현재까지 최대
+'''
+
+datasets = load_wine()
+
+print(datasets.DESCR)
+print(datasets.feature_names)
+
+x = datasets.data
+y = datasets.target
+
+print(x.shape) # (178, 13)
+print(y.shape) # (178, )
+
+y = to_categorical(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+test_size=0.7, random_state=9, shuffle=True)
+
+scaler = QuantileTransformer()
+scaler.fit(x_train)
+scaler.transform(x_train)
+scaler.transform(x_test)
+
+# modeling
+model = Sequential()
+model.add(Dense(1000, input_shape=(13, ), activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(60, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(3, activation='softmax')) 
+
+# compile
+model.compile(loss='categorical_crossentropy', optimizer='adam', 
+metrics=['accuracy'])
+
+es = EarlyStopping(monitor='loss', patience=20, mode='min', verbose=1)
+
+hist = model.fit(x_train, y_train, epochs=1000, batch_size=8, validation_split=0.2,
+callbacks=[es])
+
+# evaluate
+
+loss = model.evaluate(x_test, y_test)
+print('loss: ', loss[0])
+print('accuracy: ', loss[1])
+
+y_pred = model.predict(x_test[:5])
+# y_pred = model.predict(x_test[-5:-1])
+print(y_test[:5])
+# print(y_test[-5:-1])
+print('--------softmax를 통과한 값 --------')
+print(y_pred)
+
+# 범위 -> [:5]
+
+# scaler 없이
+# loss:  0.750230073928833
+# accuracy:  0.671999990940094
+
+
+# MinMax scaler 사용할 때
+# loss:  0.5610019564628601
+# accuracy:  0.7839999794960022
+
+# -> MinMax Scaler인데 node 조정 후
+# loss:  0.24176639318466187
+# accuracy:  0.9279999732971191
+
+# PowerTransformer
+# loss:  0.8162326812744141
+# accuracy:  0.6320000290870667
+
+# QuantileTransformer
+# loss:  0.6085973381996155
+# accuracy:  0.6800000071525574
+
+# 범위 -> [-5: -1]
+# loss:  0.6953398585319519
+# accuracy:  0.6320000290870667
