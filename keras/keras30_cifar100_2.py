56c53be98edd7dbf12697b56bf3c95ec743bccb6
yunnyisgood <cyeon0801@gmail.com>
2021-07-18 23:54:58 +0900

    today commit

--- keras/keras30_cifar100_2.py
+++ keras/keras30_cifar100_2.py
@@ -1,9 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import cifar100
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
-from sklearn.preprocessing import MinMaxScaler, PowerTransformer, QuantileTransformer, StandardScaler
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPool2D
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.utils import to_categorical
 
@@ -21,32 +19,37 @@ print(np.unique(y_train)) # [0 1 2 3 4 5 6 7 8 9]
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-# modeling
+print(y_train.shape, y_test.shape)
+
+
 # modeling
 model = Sequential()
-model.add(Conv2D(filters=100, kernel_size=(2, 2), 
-                    padding='same', input_shape=(28, 28, 3)))
-model.add(Conv2D(100, (2,2), activation='relu'))   
-model.add(Conv2D(64, (2,2), activation='relu'))   
-model.add(Conv2D(64, (2,2), activation='relu'))  
-model.add(MaxPooling2D()) 
-model.add(Conv2D(32, (2,2), activation='relu')) 
-model.add(Conv2D(32, (2,2), activation='relu')) 
-model.add(MaxPooling2D()) 
+model.add(Conv2D(100, kernel_size=(2, 2), 
+                    padding='same', input_shape=(32, 32, 3), activation='relu'))
+model.add(Conv2D(100, (2,2), padding='same', activation='relu'))   
+model.add(Conv2D(64, (2,2), padding='same', activation='relu'))   
+model.add(Conv2D(64, (2,2),padding='same', activation='relu'))  
+model.add(MaxPool2D()) 
+model.add(Conv2D(32, (2,2), padding='same', activation='relu')) 
+model.add(Conv2D(32, (2,2), padding='same', activation='relu')) 
+model.add(MaxPool2D()) 
 model.add(Flatten()) 
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(100, activation='softmax'))
 
 # compile
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='acc')
 
-es = EarlyStopping(monitor='loss', patience=20, verbose=1, mode='min')
+es = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='auto')
 
 model.fit(x_train, y_train, epochs=1000, verbose=1, callbacks=[es], validation_split=0.01,
-shuffle=True)
+shuffle=True, batch_size=200)
 
 # evaluate 
 loss = model.evaluate(x_test, y_test)
-print('loss: ', loss[0])
+print('category: ', loss[0])
 print('accuracy: ', loss[1])
+
+# category:  2.8261590003967285
+# accuracy:  0.31790000200271606
--- keras/keras30_cifar10_2.py
+++ keras/keras30_cifar10_2.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPool2D
 from sklearn.preprocessing import MinMaxScaler, PowerTransformer, QuantileTransformer, StandardScaler
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.utils import to_categorical
@@ -21,30 +21,34 @@ print(np.unique(y_train)) # [0 1 2 3 4 5 6 7 8 9]
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-# modeling
+print(y_train.shape, y_test.shape)
+
+
 # modeling
 model = Sequential()
 model.add(Conv2D(filters=100, kernel_size=(2, 2), 
-                    padding='same', input_shape=(32, 32, 3)))
+                    padding='same', input_shape=(32, 32, 3), activation='relu'))
 model.add(Conv2D(100, (2,2), activation='relu'))   
 model.add(Conv2D(64, (2,2), activation='relu'))   
 model.add(Conv2D(64, (2,2), activation='relu'))  
-# model.add(MaxPooling2D()) 
+model.add(MaxPool2D()) 
 model.add(Conv2D(32, (2,2), activation='relu')) 
 model.add(Conv2D(32, (2,2), activation='relu')) 
-model.add(MaxPooling2D()) 
+model.add(MaxPool2D()) 
 model.add(Flatten()) 
-model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))         
 model.add(Dense(32, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
+model.summary()
+
 # compile
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
 
-es = EarlyStopping(monitor='loss', patience=10, verbose=1, mode='min')
+es = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='auto')
 
-model.fit(x_train, y_train, epochs=1000, verbose=1, callbacks=[es], validation_split=0.01,
-shuffle=True, batch_size=8)
+model.fit(x_train, y_train, epochs=1000,callbacks=[es], validation_split=0.01,
+shuffle=True, batch_size=100)
 
 # evaluate 
 loss = model.evaluate(x_test, y_test)
